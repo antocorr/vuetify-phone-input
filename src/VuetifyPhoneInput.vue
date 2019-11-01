9fe19effe11786cb890898ce2878bcfdd904fda9
Bart <bartramaekers99@gmail.com>
2019-11-01 16:32:41 +0100

    Generate prepend slot

--- src/VuetifyPhoneInput.vue
+++ src/VuetifyPhoneInput.vue
@@ -16,9 +16,58 @@
                 type: String,
                 default: () => 'text',
             },
+            placeholder: {
+                type: String,
+                default: () => 'Input your phone number here'
+            },
             counter: {
                 type: Number,
                 default: () => 13,
+            },
+            countryCode: {
+                type: String,
+                default: () => '31',
+            },
+            countryCodeRequired: {
+                type: Boolean,
+                default: () => false,
+            },
+            prependCountryCode: {
+                type: Boolean,
+                default: () => true,
+            },
+            allowCharacteristics: {
+                type: Boolean,
+                default: () => false,
+            },
+            disableRules: {
+                type: Boolean,
+                default: () => false,
+            },
+        },
+        computed: {
+            // defaultRules() {
+            //     if (this.disableRules) return [...this.rules];
+            //
+            //     return [
+            //         () => this.matchesCount() || 'Count does not match',
+            //         () => this.matchesCountryCode() || 'Country code does not match',
+            //         ...this.rules];
+            // },
+            // matchesCount() {
+            //     return this.counter > this.internalValue;
+            // },
+            // matchesCountryCode() {
+            //     if (!this.countryCodeRequired) return true;
+            //
+            //     return (this.internalValue as string).startsWith(this.countryCode);
+            // },
+        },
+        methods: {
+            genPrependSlot() {
+                if (!this.prependCountryCode) return false;
+
+                return this.genSlot('prepend', 'outer', '+' + this.countryCode);
             },
         },
     });
