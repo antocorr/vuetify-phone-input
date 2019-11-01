a19e329296c15c12fc5f2fbc4803656b8013a974
Bart <bartramaekers99@gmail.com>
2019-11-01 14:44:13 +0100

    Add package structure

---
+++ src/VuetifyPhoneInput.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import VTextField from 'vuetify/lib/components/VTextField';
+    import {PropValidator} from 'vue/types/options.js';
+
+    export default VTextField.extend({
+        name: 'VuetifyPhoneInput',
+        props: {
+            value: {
+                type: String,
+                default: () => '',
+                validator: (val) => {
+                    return typeof val === 'string';
+                },
+            } as PropValidator<string>,
+            type: {
+                type: String,
+                default: () => 'text',
+            },
+            counter: {
+                type: Number,
+                default: () => 13,
+            },
+        },
+    });
+</script>
