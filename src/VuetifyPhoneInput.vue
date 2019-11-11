8fb18a16b15d53e8f8ec6dc141d24b992b87f531
Bart <bartramaekers99@gmail.com>
2019-11-11 15:15:56 +0100

    Format phone number on input

--- src/VuetifyPhoneInput.vue
+++ src/VuetifyPhoneInput.vue
@@ -47,15 +47,16 @@
         },
         watch: {
             value: {
-                handler(value) {
-                    this.parsePhoneNumber(value)
+                handler(value: string) {
+                    this.setPhoneNumber(
+                        value.replace(new RegExp('^00'), '+')
+                    )
                 },
                 immediate: true,
             },
             countryCode: {
-                handler(code) {
-                    this.internalCountryCode = code;
-                    this.parsePhoneNumber(this.internalValue);
+                handler(countryCode: [string, number]) {
+                    this.setPhoneNumber('+' + countryCode + this.internalValue);
                 },
                 immediate: true,
             },
@@ -69,6 +70,8 @@
             genSelector() {
                 const selections = this.countries.length ? this.countries : countries.sort((a, b) => Number(a.code) - Number(b.code));
 
+                if (selections.length === 0) return;
+
                 return this.$createElement(VSelect, {
                     props: {
                         items: selections.map((el) => {
@@ -78,7 +81,8 @@
                             }
                         }),
                         placeholder: this.placeholder,
-                        value: this.internalCountryCode,
+                        value: this.internalCountryCode || selections[0].code,
+                        disabled: selections.length == 1,
                     },
                     on: {
                         input: this.selectCountry,
@@ -94,28 +98,40 @@
             onInput(e) {
                 VTextField.options.methods.onInput.call(this, e);
 
-                this.$emit('phone', this.internalValue);
+                this.setPhoneNumber(e.target.value);
             },
             onBlur(e) {
                 VTextField.options.methods.onBlur.call(this, e);
 
-                this.parsePhoneNumber(e.target.value);
-            },
-            prefixCountryCode() {
-                return '+' + this.internalCountryCode + this.internalValue;
+                this.setPhoneNumber(e.target.value);
+
+                this.$emit('phone', this.preparePhoneNumber(this.internalValue));
             },
             selectCountry(code: string) {
                 this.$emit('country', this.internalCountryCode = code);
 
-                if (this.returnWithCountryCode) this.$emit('phone', this.prefixCountryCode());
+                if (this.returnWithCountryCode) this.$emit('phone', this.preparePhoneNumber(this.internalValue));
             },
-            parsePhoneNumber(phone: string) {
-                const phoneNumber = parsePhoneNumberFromString(phone);
+            preparePhoneNumber(phone: string): string {
+                let final = phone;
+                
+                if (!this.prependCountryCode) return this.internalCountryCode + phone;
 
-                if (phoneNumber) {
-                    this.lazyValue = phoneNumber.nationalNumber;
-                    this.internalCountryCode = Number(phoneNumber.countryCallingCode);
+                if (!final.startsWith('+')) {
+                    final = '+' + this.internalCountryCode + phone;
                 }
+
+                return final.split(' ').join('');
+            },
+            setPhoneNumber(phone: string) {
+                const phoneNumber = parsePhoneNumberFromString(this.preparePhoneNumber(phone), this.internalCountryCode.toString());
+
+                if (!phoneNumber) return;
+
+                if (!phoneNumber.isValid()) return;
+
+                this.internalValue = phoneNumber.formatNational();
+                this.internalCountryCode = Number(phoneNumber.countryCallingCode);
             },
         },
     });
