f8ecbd8293be3fe3646e806ba2af36e70eb8578d
ricardovanlaarhoven <ricardo1994@gmail.com>
2020-11-16 18:52:05 +0100

    remove install from package

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
 import VuetifyPhoneInput from './VuetifyPhoneInput.vue'
 
-const install = (Vue: any, options: {}) => {
-    Vue.component('VuetifyPhoneInput', VuetifyPhoneInput);
-};
-
-export default {
-    install,
-    VuetifyPhoneInput,
-};
+export default VuetifyPhoneInput;
